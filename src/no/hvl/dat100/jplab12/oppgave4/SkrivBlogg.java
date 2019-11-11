c631b3564767d993ea6feb81cef730118a27cb9a
588405 <frede.berdal@gmail.com>
2019-11-11 13:04:16 +0100

    Add files via upload
    Gjort oppgaver

--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -2,16 +2,26 @@ package no.hvl.dat100.jplab12.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
 import no.hvl.dat100.jplab12.oppgave3.*;
 import no.hvl.dat100.jplab12.common.TODO;
 
 public class SkrivBlogg {
-
 	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
 
 	public static boolean skriv(Blogg samling, String filnavn) {
+		PrintWriter skriver = null;
 
-		throw new UnsupportedOperationException(TODO.method());
+		try {
+			skriver = new PrintWriter(MAPPE + filnavn);
+			skriver.println(samling.toString());
+		} catch (FileNotFoundException e) {
+			return false;
+		} finally {
+			if (skriver != null) {
+				skriver.close();
+				return true;
+			}
+		}
+		return false;
 	}
 }
